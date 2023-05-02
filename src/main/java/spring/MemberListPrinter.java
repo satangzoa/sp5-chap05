fda0288c9f2d3dc195e5c5b63f5bce7eb8ae6824
A <A@DESKTOP-NKSGQVR>
2023-05-02 18:19:00 +0900

    spring5

---
+++ src/main/java/spring/MemberListPrinter.java
@@ -0,0 +1,23 @@
+package spring;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Component;
+
+@Component("listPrinter")
+public class MemberListPrinter {
+
+	private MemberDao memberDao;
+	private MemberPrinter printer;
+
+	public MemberListPrinter(MemberDao memberDao, MemberPrinter printer) {
+		this.memberDao = memberDao;
+		this.printer = printer;
+	}
+
+	public void printAll() {
+		Collection<Member> members = memberDao.selectAll();
+		members.forEach(m -> printer.print(m));
+	}
+
+}
